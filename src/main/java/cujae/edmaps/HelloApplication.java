d2c90904097f82e5a6654dab7597dbeacad9f8bf
Daniel Chico <106417219+MurderShade@users.noreply.github.com>
2022-12-08 13:11:46 -0500

    userHistory: modify a bus stop name

--- src/main/java/cujae/edmaps/HelloApplication.java
+++ src/main/java/cujae/edmaps/HelloApplication.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.InvalidParameterException;
 
 public class HelloApplication extends Application {
     @Override
@@ -58,9 +59,24 @@ public class HelloApplication extends Application {
         //user story 2: remove a bus and all its routes/edges
         havana.removeBus("Route-3");
     }
+    public static void userHistoryModifyBusStopName(){
+        City havana = new City("Havana");
+        havana.addBusStop("Airport");
+        havana.addBusStop("Train Station");
+        try {
+            havana.modifyBusStopName("Airport", "José Martí Airport");
+            System.out.println("ok!");
+            havana.modifyBusStopName("José Martí Airport", "Train Station");
+            System.out.println(":(");
+        }catch(InvalidParameterException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 
     public static void main(String[] args) {
         userHistoryCityCreation();
+        userHistoryModifyBusStopName();
         launch();
     }
 }
