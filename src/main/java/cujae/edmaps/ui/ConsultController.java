7f32680f989e67b9413d5ca7a2f540080a6a7c66
VicentGJ <vsgarofalo@gmail.com>
2022-12-09 18:08:02 -0500

    feat: Implemented ConsultController, without displaying the results yet

---
+++ src/main/java/cujae/edmaps/ui/ConsultController.java
@@ -0,0 +1,52 @@
+package cujae.edmaps.ui;
+
+import cujae.edmaps.core.BusStop;
+import cujae.edmaps.core.City;
+import cujae.edmaps.core.dijkstra.CompletePath;
+import cujae.edmaps.core.dijkstra.Path;
+import cujae.edmaps.utils.ViewLoader;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConsultController implements Initializable {
+
+    @FXML
+    private ComboBox<String> startCombobox, destinationComboBox;
+
+    private ObservableList<String> stopList;
+    private final City city = City.getInstance();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (BusStop stop: city.getBusStopList()) {
+            stopList.add(stop.getName());
+        }
+        startCombobox.setItems(stopList);
+        destinationComboBox.setItems(stopList);
+    }
+
+    @FXML
+    public void onOkButton() {
+        try {
+            CompletePath path = city.getPathBetween(startCombobox.getValue(), destinationComboBox.getValue());
+            for (Path p : path.getPaths()) {
+                System.out.println(p.getBus() != null ? p.getBus().getName() : null);
+                System.out.println(p.getDistance());
+                System.out.println(((BusStop) p.getStop().getInfo()).getName());
+                System.out.println(":::::::::::::::::");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onCancelButton() {
+        ViewLoader.getStage().close();
+    }
+}
