115a8c4d1704cf52e63321dd944276a567dde7b4
VicentGJ <vsgarofalo@gmail.com>
2022-12-09 14:34:00 -0500

    feat: getStage function implemented in ViewLoader

--- src/main/java/cujae/edmaps/utils/ViewLoader.java
+++ src/main/java/cujae/edmaps/utils/ViewLoader.java
@@ -12,26 +12,32 @@ import java.net.URL;
 import java.util.Objects;
 
 public class ViewLoader {
+
+    private static Stage stage;
+
     public static Object newWindow(URL loc, String title, Stage parentStage) throws IOException {
         Object controller = null;
         FXMLLoader loader = new FXMLLoader(loc);
-        Parent parent = loader.load();
-        controller = loader.getController();
-        Stage stage = null;
         stage = Objects.requireNonNullElseGet(parentStage, () -> new Stage(StageStyle.DECORATED));
         stage.setTitle(title);
-        stage.setScene(new Scene(parent));
+        stage.setScene(new Scene(loader.load()));
         stage.show();
+        controller = loader.getController();
         return controller;
     }
+
     public static Object thisWindow(URL fxmlResource, ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(fxmlResource);
         Parent next_page_parent = loader.load();
         Object controller = loader.getController();
         Scene next_page_scene = new Scene(next_page_parent);
-        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        app_stage.setScene(next_page_scene);
-        app_stage.show();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(next_page_scene);
+        stage.show();
         return controller;
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 }
