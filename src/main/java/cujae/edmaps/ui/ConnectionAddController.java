6c8704ee292f4b7adf0dc6abeb2773d3043131d2
CarlosD <carlosd.vilaseca@avangenio.com>
2022-12-10 10:05:43 -0500

    fix: spinner value behavior

--- src/main/java/cujae/edmaps/ui/ConnectionAddController.java
+++ src/main/java/cujae/edmaps/ui/ConnectionAddController.java
@@ -20,7 +20,7 @@ public class ConnectionAddController implements Initializable {
     @FXML
     ComboBox<String> stop1ComboBox, stop2ComboBox, busComboBox;
     @FXML
-    Spinner<Float> distanceSpinner;
+    Spinner distanceSpinner;
 
     ObservableList<String> stopList = FXCollections.observableArrayList();
     ObservableList<String> busList = FXCollections.observableArrayList();
@@ -28,7 +28,8 @@ public class ConnectionAddController implements Initializable {
 
     @FXML
     public void onOkButton() {
-        city.insertRoute(stop1ComboBox.getValue(), stop2ComboBox.getValue(), busComboBox.getValue(), distanceSpinner.getValue());
+        Float distance = Float.parseFloat(String.valueOf(distanceSpinner.getValueFactory().getValue()));
+        city.insertRoute(stop1ComboBox.getValue(), stop2ComboBox.getValue(), busComboBox.getValue(), distance);
         ViewLoader.getStage().close();
     }
 
@@ -39,10 +40,10 @@ public class ConnectionAddController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        for (BusStop stop: city.getBusStopList()) {
+        for (BusStop stop : city.getBusStopList()) {
             stopList.add(stop.getName());
         }
-        for (Bus bus: city.getBusList()) {
+        for (Bus bus : city.getBusList()) {
             busList.add(bus.getName());
         }
         stop1ComboBox.setItems(stopList);
