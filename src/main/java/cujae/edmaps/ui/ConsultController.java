a237f98808329ce7ec9026ece4d5c0215d403255
VicentGJ <vsgarofalo@gmail.com>
2022-12-09 23:14:12 -0500

    feat: implemented table view to show results in consult-shortest-path.fxml

--- src/main/java/cujae/edmaps/ui/ConsultController.java
+++ src/main/java/cujae/edmaps/ui/ConsultController.java
@@ -5,12 +5,16 @@ import cujae.edmaps.core.City;
 import cujae.edmaps.core.dijkstra.CompletePath;
 import cujae.edmaps.core.dijkstra.Path;
 import cujae.edmaps.utils.ViewLoader;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,9 +25,32 @@ public class ConsultController implements Initializable {
     private ComboBox<String> startComboBox, destinationComboBox;
     @FXML
     private Label totalDistanceDisplay;
+    @FXML
+    private TableView<PathHelper> tableView;
+    @FXML
+    private TableColumn stopColumn, busColumn;
 
     private final ObservableList<String> stopList = FXCollections.observableArrayList();
     private final City city = City.getInstance();
+
+    public static class PathHelper {
+         private final SimpleStringProperty stopName;
+         private final SimpleStringProperty busName;
+
+         private PathHelper(String stopName, String busName) {
+             this.stopName = new SimpleStringProperty(stopName);
+             this.busName = new SimpleStringProperty(busName);
+         }
+
+         public String getStopName() {
+             return stopName.get();
+         }
+
+         public String getBusName() {
+             return busName.get();
+         }
+
+     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -37,6 +64,7 @@ public class ConsultController implements Initializable {
     @FXML
     public void onOkButton() {
         try {
+            ObservableList<PathHelper> pathList = FXCollections.observableArrayList();
             CompletePath path = city.getPathBetween(startComboBox.getValue(), destinationComboBox.getValue());
             for (Path p : path.getPaths()) {
                 System.out.println(p.getBus() != null ? p.getBus().getName() : null);
@@ -44,7 +72,16 @@ public class ConsultController implements Initializable {
                 System.out.println(((BusStop) p.getStop().getInfo()).getName());
                 System.out.println(":::::::::::::::::");
                 totalDistanceDisplay.setText(p.getDistance().toString());
+                pathList.add(new PathHelper(((
+                        BusStop) p.getStop().getInfo()).getName(),
+                        p.getBus() != null ? p.getBus().getName() : "Start"
+                ));
             }
+            stopColumn.setCellValueFactory(
+                    new PropertyValueFactory<PathHelper, String>("stopName"));
+            busColumn.setCellValueFactory(
+                    new PropertyValueFactory<PathHelper, String>("busName"));
+            tableView.setItems(pathList);
         } catch (Exception e) {
             e.printStackTrace();
         }
