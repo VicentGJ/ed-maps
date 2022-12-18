2f462241b7f35551ba0d9cf747b05164ff045e46
CarlosD <carlosd.vilaseca@avangenio.com>
2022-12-18 00:47:00 -0500

    feat: filter to show only the buses that connect both stops in the connection delete view

--- src/main/java/cujae/edmaps/ui/ConnectionDeleteController.java
+++ src/main/java/cujae/edmaps/ui/ConnectionDeleteController.java
@@ -3,10 +3,7 @@ package cujae.edmaps.ui;
 import cu.edu.cujae.ceis.graph.edge.Edge;
 import cu.edu.cujae.ceis.graph.edge.WeightedEdge;
 import cu.edu.cujae.ceis.graph.vertex.Vertex;
-import cujae.edmaps.core.Bus;
-import cujae.edmaps.core.BusStop;
-import cujae.edmaps.core.City;
-import cujae.edmaps.core.MapsManager;
+import cujae.edmaps.core.*;
 import cujae.edmaps.utils.Drawer;
 import cujae.edmaps.utils.ViewLoader;
 import javafx.collections.FXCollections;
@@ -18,6 +15,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -27,7 +25,6 @@ public class ConnectionDeleteController implements Initializable {
     ComboBox<String> stop1ComboBox, stop2ComboBox, busComboBox;
 
     ObservableList<String> stopList = FXCollections.observableArrayList();
-    ObservableList<String> busList = FXCollections.observableArrayList();
     private final City city = MapsManager.getInstance().getActualCity();
 
     @Override
@@ -38,39 +35,35 @@ public class ConnectionDeleteController implements Initializable {
         stop1ComboBox.setItems(stopList);
         stop2ComboBox.setItems(stopList);
         busComboBox.setDisable(true);
-        busComboBox.getItems().add("Walking");
-        busComboBox.setValue("Walking");
 
         stop1ComboBox.setOnAction(event -> {
+            LinkedList<String> busList = new LinkedList<>();
             if (stop2ComboBox.getValue() != null) {
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                for (Bus b : city.busFilter(stop1ComboBox.getValue(), stop2ComboBox.getValue())) {
-                    busList.add(b.getName());
+                for (Edge e : city.getConnectingEdges(stop1ComboBox.getValue(), stop2ComboBox.getValue())) {
+                    Bus bus = ((Route) ((WeightedEdge) e).getWeight()).getBus();
+                    if (bus != null) busList.add(bus.getName());
+                    else busList.add("Walking");
                 }
-                busComboBox.setDisable(false);
+                busComboBox.setDisable(busList.isEmpty());
                 busComboBox.getItems().clear();
-                busComboBox.getItems().add("Walking");
-                busComboBox.setValue("Walking");
                 busComboBox.getItems().addAll(busList);
             } else {
                 busComboBox.setDisable(true);
-                busComboBox.setValue("Walking");
             }
         });
         stop2ComboBox.setOnAction(event -> {
+            LinkedList<String> busList = new LinkedList<>();
             if (stop1ComboBox.getValue() != null) {
-                System.out.println("2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                for (Bus b : city.busFilter(stop1ComboBox.getValue(), stop2ComboBox.getValue())) {
-                    busList.add(b.getName());
+                for (Edge e : city.getConnectingEdges(stop1ComboBox.getValue(), stop2ComboBox.getValue())) {
+                    Bus bus = ((Route) ((WeightedEdge) e).getWeight()).getBus();
+                    if (bus != null) busList.add(bus.getName());
+                    else busList.add("Walking");
                 }
-                busComboBox.setDisable(false);
+                busComboBox.setDisable(busList.isEmpty());
                 busComboBox.getItems().clear();
-                busComboBox.getItems().add("Walking");
-                busComboBox.setValue("Walking");
                 busComboBox.getItems().addAll(busList);
             } else {
                 busComboBox.setDisable(true);
-                busComboBox.setValue("Walking");
             }
         });
     }
