179cf81a78304a1c6d2cdb14a9f23202c0660f05
Carlos Daniel Vilaseca <carlosd.vilaseca@generalsoftwareinc.com>
2022-12-18 16:19:29 -0500

    some new validations (#39)
    * fix: default value for combobox in simple delete view
    
    * fix: disable ok button if element to add already exists
    
    Co-authored-by: CarlosD <carlosd.vilaseca@avangenio.com>

--- src/main/java/cujae/edmaps/ui/SimpleAddController.java
+++ src/main/java/cujae/edmaps/ui/SimpleAddController.java
@@ -1,15 +1,18 @@
 package cujae.edmaps.ui;
 
 import cujae.edmaps.core.City;
+import cujae.edmaps.core.FileManager;
 import cujae.edmaps.core.MapsManager;
 import cujae.edmaps.utils.Drawer;
 import cujae.edmaps.utils.ViewLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +23,8 @@ public class SimpleAddController implements Initializable {
     private TextField nameField;
     private AddType type = AddType.STOP;
     private final City city = MapsManager.getInstance().getActualCity();
+    @FXML
+    Button okButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -30,27 +35,54 @@ public class SimpleAddController implements Initializable {
             case "Add Stop" -> type = AddType.STOP;
             case "Add City" -> type = AddType.CITY;
         }
+        okButton.setDisable(true);
+        nameField.setOnKeyTyped(event -> {
+            boolean exists = false;
+            switch (type) {
+                case CITY -> {
+                    if (!nameField.getText().isBlank()) {
+                        File[] cities = FileManager.getAllConsultDirectories();
+                        for (File f : cities) {
+                            if (f.getName().equalsIgnoreCase(nameField.getText())) {
+                                exists = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                case BUS -> {
+                    if (!nameField.getText().isBlank()) {
+                        exists = MapsManager.getInstance().getActualCity().existBus(nameField.getText());
+                    }
+                }
+                case STOP -> {
+                    if (!nameField.getText().isBlank()) {
+                        exists = MapsManager.getInstance().getActualCity().existBusStop(nameField.getText());
+                    }
+                }
+            }
+            okButton.setDisable(exists);
+        });
     }
 
     @FXML
     public void onOkButton(ActionEvent event) throws IOException {
         try {
             switch (type) {
-                case BUS:
-                    city.addBus(nameField.getText());
-                    break;
-                case STOP:
+                case BUS -> city.addBus(nameField.getText());
+                case STOP -> {
                     city.addBusStop(nameField.getText());
                     Drawer drawer = Drawer.getInstance();
                     MainController.setGraphContainer(drawer.draw(null, null));
-                    break;
-                case CITY:
+                }
+                case CITY -> {
                     MapsManager.getInstance().createCity(nameField.getText());
-                    break;
+                    Drawer drawer = Drawer.getInstance();
+                    MainController.setGraphContainer(drawer.draw(null, null));
+                }
             }
             ViewLoader.getStage().close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             PopupController controller = (PopupController) ViewLoader.newWindow(getClass().getResource("popup.fxml"), "Error", null);
             controller.setText("Error", e.getMessage());
             controller.setPrevious("add-form.fxml", ((Stage) nameField.getScene().getWindow()).getTitle());
